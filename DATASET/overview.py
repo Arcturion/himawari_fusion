4f2220fc4f139df1ea9df9fd3709b83db26826c6
Arcturion <dimascours@gmail.com>
2022-11-21 21:47:46 +0800

    Update overview.py

--- DATASET/overview.py
+++ DATASET/overview.py
@@ -16,10 +16,14 @@ files = glob.glob(r'*.DAT')
 #scn = Scene(filenames=files,  reader='ahi_hsd',filter_parameters={'start_time': datetime(2021,5,25,2,00), 'end_time': datetime(2021,5,25,2,10)})
 scn = Scene(filenames=files,  reader='ahi_hsd')
 
-scn.load(["B03"])
-scn.save_dataset('B02', 'B02.png')
+#https://github.com/JeanPaulKoole/Satelliet/blob/135843d9f566398b926ffe75897d7dac01569f6f/Satelliet-2.ipynb
+scn.show("B03")
+scn['B03'].plot.imshow()
 
-image = plt.imread('B03_1.png') 
+scn.load(["B03"])
+scn.save_dataset('B03', 'B03.png')
+
+image = plt.imread('B03.png') 
 plt.imshow(image)
 cbar=plt.colorbar()
 cbar.set_label("Kelvin")
